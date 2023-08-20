22d6390cda3e47e5d46ecf1e0b3337fabaa526e1
Rainer Flores <rainerneno@gmail.com>
2023-08-21 00:10:04 +0800

    pages folder created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Main from "./components/Main";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
